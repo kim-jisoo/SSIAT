d2fae4bf3e518c47c477e3b285ae84b8fb0e5e2f
jisoo kim <jisookim20032003@gmail.com>
2021-07-01 13:47:54 -0700

    Login page started

---
+++ src/pages/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import LoginImage from '../images/login-2.png';
+import Logo from '../images/NavBarLogo.png';
+import GoogleIcon from '../images/google-icon.png';
+
+import './Login.css';
+function Login () {
+    return( 
+        <div className="login-page">
+            <div className="login-left-panel">
+                <div className='login-left-panel-2'>
+                    <text className="login-title">씨앗 커뮤니티에 오신 것을 환영합니다!</text>
+                    <button className='google-button'>
+                        <img src={GoogleIcon} className='google-icon' alt='google'/>
+                        <text className='google-text'>구글 어카운트로 로그인</text>
+                    </button>
+                    
+                    <text>이메일 주소</text>
+                    
+                </div>
+            </div>
+            <div className="login-right-panel">
+                <img src={Logo} alt="logo" className='login-logo' />
+                <img src={LoginImage} alt="login" className='login-image'/>
+            </div>
+          
+        </div>
+    )
+}
+
+export default Login;
