481629f5ae02b7df55e32159f2718aed29fdb966
jisoo kim <jisookim20032003@gmail.com>
2021-07-26 15:32:03 -0700

    completed congrats page

--- src/CongratsPage/CongratsPage.js
+++ src/CongratsPage/CongratsPage.js
@@ -1,13 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import Banner from '../data/_images/congrats.png';
-
+import Background from '../data/_images/congrats.png';
 import './CongratsPage.css'
+
+// const [error, setError] = useState(null);
+// const [isLoaded, setIsLoaded] = useState(false);
+// const [items, setItems] = useState([]);
+
+// useEffect(() => {
+//     fetch("https://api.example.com/items")
+//     .then(res => res.json())
+//     .then(
+//       (result) => {
+//         setIsLoaded(true);
+//         setItems(result);
+//       },
+//       // Note: it's important to handle errors here
+//       // instead of a catch() block so that we don't swallow
+//       // exceptions from actual bugs in components.
+//       (error) => {
+//         setIsLoaded(true);
+//         setError(error);
+//       }
+//     )
+// }, [])
+
 function CongratsPage() {
     return (
-        <div className="congrats-container">
-            <img src={Banner} className="congrats-banner" />
-            <title className="congrats-title"> Congratulations! </title>
+        <div className="congrats-container" style={{ backgroundImage: `url(${Background})`, height: '1080px'}}>
+            <h1 className="congrats-title"> Congratulations! </h1>
+            {/* 
+                <h2 className="congrats-name"> {user.name} 님, </h2>
+            */}
+            <h2 className="congrats-name"> 홍길동 님,</h2>
+            <h2 className="congrats-line" > 씨드그룹 가입을 축하합니다! </h2>
+            <Link to="/question-1" className="btn congrats-button" > 시작하기</Link>
         </div>
     )
 }
